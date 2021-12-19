77fe0ec2cf74c39d5e64a2325c9e075ca374c8cc
harun ugur <ugur.harun@yandex.com>
2021-12-19 14:11:27 +0300

    Single File Components

---
+++ src/views/EventList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="events">
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <EventCard v-for="event in events" :key="event.id" :event = "event"/>
+  </div>
+</template>
+
+<script>
+import EventCard from "@/components/EventCard";
+
+export default {
+  name: "EventList",
+  components: {
+    EventCard,
+  },
+  data () {
+    return {
+      events :[
+          {
+            id : 1,
+            title : "Play Tennis",
+            date : "2021-01-10",
+            time : "15:30"
+          },
+          {
+            id : 2,
+            title : "Setup Meeting",
+            date : "2021-02-10",
+            time : "08:30"
+          }
+         ]
+    }
+    
+  }
+};
+</script>
+<style scoped>
+.events{
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+}
+</style>
